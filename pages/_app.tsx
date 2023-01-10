a6726c0481505f82c2493ceb4ce4507746e289a0
Marvin Mante <marvin.mante@nextroll.com>
2023-01-10 10:02:52 -0800

    add fix for fouc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-// import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import React from 'react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  React.useEffect(() => {
+    const removeFouc = (foucElement:Element) => {
+      foucElement.className = foucElement.className.replace('no-fouc', 'fouc');
+    };
+  
+    removeFouc(document.documentElement);
+  }, []);
+
   return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="no-fouc">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Gloria+Hallelujah&family=Lexend:wght@100;200;300;400&family=Permanent+Marker&display=swap" rel="stylesheet"/>
       </Head>
